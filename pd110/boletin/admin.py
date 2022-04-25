79832e0ac2356ce1297d2b06c508afdcd0541f82
pedropegu <pgpedro0018@gmail.com>
2022-04-25 08:57:15 +0200

    Videos 16-17

--- pd110/boletin/admin.py
+++ pd110/boletin/admin.py
@@ -1,16 +1,18 @@
 import email
 from django.contrib import admin
 from .models import Registrado
+from .forms import RegModelForm, ReggForm
 # Register your models here.
 
 class AdminRegistrado(admin.ModelAdmin):
+    form = RegModelForm
     list_display = ['email', 'nombre', 'timestamp']
     #list_display_links = ['nombre']
     list_filter = ['timestamp']
     list_editable = ['nombre']
     search_fields = ['email','nombre']
-    class Meta:
-        model = Registrado
+    # class Meta:
+    #     model = Registrado
 
 
 admin.site.register(Registrado, AdminRegistrado)
--- pd110/boletin/forms.py
+++ pd110/boletin/forms.py
@@ -1,6 +1,22 @@
-from xml.dom import NoModificationAllowedErr
-from django import forms
 
+from distutils import extension
+from django import forms
+from .models import Registrado
+
+
+class RegModelForm(forms.ModelForm):
+    class Meta:
+        model = Registrado
+        fields = ["email","nombre"]
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        email_base, proveedor = email.split("@")
+        dominio, extension = email.split(".")
+        if not extension == "edu":
+            raise forms.ValidationError("Use la extensión .EDU")
+        return email
+
+    
 class ReggForm(forms.Form):
     nombre = forms.CharField(max_length=100)
     email = forms.EmailField()
