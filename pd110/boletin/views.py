04701cacaef93d9e54fa94afd8a3328d2aaa88d3
pedropegu <pgpedro0018@gmail.com>
2022-04-25 09:17:02 +0200

    Videos 18-19

--- pd110/boletin/views.py
+++ pd110/boletin/views.py
@@ -2,17 +2,45 @@ import email
 import re
 from types import MethodDescriptorType, MethodType
 from django.shortcuts import render
-from .forms import ReggForm
+from .forms import ReggForm,RegModelForm
 from .models import Registrado
 # Create your views here.
 def inicio(request):
-    form = ReggForm(request.POST or None)
-    if form.is_valid():
-        form_data = form.cleaned_data
-        correo = form_data.get("email")
-        usuario = form_data.get("nombre")
-        obj = Registrado.objects.create(email=correo, nombre=usuario)
+
+    titulo = "Hola"
+    
+
+    if request.user.is_authenticated:
+        titulo = "Bienvenido {}".format(request.user)
+
+
+    form = RegModelForm(request.POST or None)
     context = {
+        "titulo":titulo,
         "form":form,
     }
+
+    if form.is_valid():
+
+        instance = form.save(commit=False)
+        nombre = form.cleaned_data.get("nombre")
+        email = form.cleaned_data.get("email")
+        
+        if not instance.nombre:
+            instance.nombre = "persona"
+        instance.save()
+
+
+        if not nombre:
+            nombre = "anonimo"
+
+        context = {
+            "titulo": "Gracias {}".format(nombre)
+        }
+
+        # form_data = form.cleaned_data
+        # correo = form_data.get("email")
+        # usuario = form_data.get("nombre")
+        # obj = Registrado.objects.create(email=correo, nombre=usuario)
+
     return render(request, "index.html", context)
